a1ced7a7a7e62e4e44d5ddea9f0b96f6e68c6223
熊哲皓 <10826321+LittleBear-1025@user.noreply.gitee.com>
2025-05-06 11:24:20 +0800

    将tab设置为4空格

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- default options that are always set: https://github.com/lazyvim/lazyvim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- 设置tab
+vim.o.tabstop = 4 -- 一个 TAB 字符显示为 4 个空格
+vim.o.expandtab = true -- 按 TAB 键会插入空格而不是 TAB 字符
+vim.o.softtabstop = 4 -- 代替 TAB 字符插入的空格数
+vim.o.shiftwidth = 4 -- 缩进时插入的空格数
--- lua/plugins/blink.cmp.lua
+++ lua/plugins/blink.cmp.lua
@@ -1,53 +1,53 @@
 -- return {}
 return {
-  {
-    "saghen/blink.compat",
-    -- use the latest release, via version = '*', if you also use the latest release for blink.cmp
-    --  Luasnip has been moved to the `snippets` source, alongside a new preset system (`snippets.preset = 'luasnip'`). See the documentation for more information.
-    -- version = "*",
-    -- lazy.nvim will automatically load the plugin when it's required by blink.cmp
-    lazy = true,
-    -- make sure to set opts so that lazy.nvim calls blink.compat's setup
-    opts = {},
-  },
-  {
-    "saghen/blink.cmp",
-    version = false,
-    build = "cargo build --release",
-    dependencies = {
-      { "saghen/blink.compat" },
-      { "Kaiser-Yang/blink-cmp-avante" },
+    {
+        "saghen/blink.compat",
+        -- use the latest release, via version = '*', if you also use the latest release for blink.cmp
+        --  Luasnip has been moved to the `snippets` source, alongside a new preset system (`snippets.preset = 'luasnip'`). See the documentation for more information.
+        -- version = "*",
+        -- lazy.nvim will automatically load the plugin when it's required by blink.cmp
+        lazy = true,
+        -- make sure to set opts so that lazy.nvim calls blink.compat's setup
+        opts = {},
     },
-    opts = {
-      keymap = {
-        preset = "super-tab",
-        ["<C-J>"] = { "show", "show_documentation", "hide_documentation" },
-      },
-      fuzzy = {
-        sorts = {
-          "exact",
-          "score",
-          "sort_text",
+    {
+        "saghen/blink.cmp",
+        version = false,
+        build = "cargo build --release",
+        dependencies = {
+            { "saghen/blink.compat" },
+            { "Kaiser-Yang/blink-cmp-avante" },
         },
-      },
-      sources = {
-        default = {
-          "avante",
-          "lsp",
-          "path",
-          "snippets",
-          "buffer",
-        },
-        providers = {
-          avante = {
-            module = "blink-cmp-avante",
-            name = "Avante",
-            opts = {
-              -- options for blink-cmp-avante
+        opts = {
+            keymap = {
+                preset = "super-tab",
+                ["<C-J>"] = { "show", "show_documentation", "hide_documentation" },
             },
-          },
+            fuzzy = {
+                sorts = {
+                    "score",
+                    "sort_text",
+                    "exact",
+                },
+            },
+            sources = {
+                default = {
+                    "avante",
+                    "lsp",
+                    "path",
+                    "snippets",
+                    "buffer",
+                },
+                providers = {
+                    avante = {
+                        module = "blink-cmp-avante",
+                        name = "Avante",
+                        opts = {
+                            -- options for blink-cmp-avante
+                        },
+                    },
+                },
+            },
         },
-      },
     },
-  },
 }
