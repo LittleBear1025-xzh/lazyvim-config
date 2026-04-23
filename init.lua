017a9c4df1da16387fe50af4a3a826270a203932
熊哲皓 <1776166092@qq.com>
2026-04-23 21:50:50 +0800

    neovide不再全屏

--- init.lua
+++ init.lua
@@ -7,5 +7,5 @@ if vim.g.neovide then
     -- vim.g.neovide_normal_opacity = 0.8
     vim.o.guifont = "Maple Mono NF CN:h13"
     vim.g.neovide_remember_window_size = true
-    vim.g.neovide_fullscreen = true
+    -- vim.g.neovide_fullscreen = true
 end
